f70110e2d5d81482e2817014b2e2d45088474e9e
Princess <b.stevenson@hotmail.com>
2021-07-21 23:17:21 +0100

    actually added the homework this time! like forgetting to attach an attachment to an email..! MVP done on traveller, one extension completed, last one unable to complete. Advanced not tried

--- hw_traveller/hw_traveller_start/models/traveller.js
+++ hw_traveller/hw_traveller_start/models/traveller.js
@@ -3,27 +3,32 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance >= minDistance);
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((runningTotal, journey) => runningTotal += journey.distance, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const ModesOfTransport = [];
+  for (const yada yada yada of this.ModesOfTransport)
+  return this.journeys.reduce((ModesOfTransport, journey) => {
+    if (this.journeys.includes(ModesOfTransport === false))  this.journey.push(ModesOfTransport)  
+     return ModesOfTransport
+   })
 };
 
 
