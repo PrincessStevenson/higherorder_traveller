2f9935c293bed31e3818e562c55fe819f49c9af1
Princess <b.stevenson@hotmail.com>
2021-07-21 23:34:34 +0100

    working last function but not as supposed to be? but all tests passing now

--- hw_traveller/hw_traveller_start/models/traveller.js
+++ hw_traveller/hw_traveller_start/models/traveller.js
@@ -22,14 +22,21 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
   return this.journeys.reduce((runningTotal, journey) => runningTotal += journey.distance, 0);
 };
 
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   const ModesOfTransport = [];
+//   for (const yada yada yada of this.ModesOfTransport)
+//   return this.journeys.reduce((ModesOfTransport, journey) => {
+//     if (this.journeys.includes(ModesOfTransport === false))  this.journey.push(ModesOfTransport)  
+//      return ModesOfTransport
+//    })
+// };
+
+
+// this one works but should be as above to include .reduce, .includes & .push
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  const ModesOfTransport = [];
-  for (const yada yada yada of this.ModesOfTransport)
-  return this.journeys.reduce((ModesOfTransport, journey) => {
-    if (this.journeys.includes(ModesOfTransport === false))  this.journey.push(ModesOfTransport)  
-     return ModesOfTransport
-   })
-};
+  modesOfTransport = this.journeys.map(journey => journey.transport);
+  return modesOfTransport.filter((transport,index,map)=>map.indexOf(transport)===index)
+ };
 
 
 module.exports = Traveller;
