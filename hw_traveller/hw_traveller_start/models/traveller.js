aa3e9675b664ea3497b0f0617df6f2235117e3c9
Princess <b.stevenson@hotmail.com>
2021-07-22 12:26:46 +0100

    last test corrected - MVP & extensions done

--- hw_traveller/hw_traveller_start/models/traveller.js
+++ hw_traveller/hw_traveller_start/models/traveller.js
@@ -22,21 +22,19 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
   return this.journeys.reduce((runningTotal, journey) => runningTotal += journey.distance, 0);
 };
 
-// Traveller.prototype.getUniqueModesOfTransport = function () {
-//   const ModesOfTransport = [];
-//   for (const yada yada yada of this.ModesOfTransport)
-//   return this.journeys.reduce((ModesOfTransport, journey) => {
-//     if (this.journeys.includes(ModesOfTransport === false))  this.journey.push(ModesOfTransport)  
-//      return ModesOfTransport
-//    })
-// };
-
-
-// this one works but should be as above to include .reduce, .includes & .push
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  modesOfTransport = this.journeys.map(journey => journey.transport);
-  return modesOfTransport.filter((transport,index,map)=>map.indexOf(transport)===index)
- };
+  return this.journeys.reduce((ModesOfTransport, journey) => {
+    if (!ModesOfTransport.includes(journey.transport)){
+      ModesOfTransport.push(journey.transport)
+    }
+
+  return ModesOfTransport
+
+   }, [])
+};
+
+
+
 
 
 module.exports = Traveller;
